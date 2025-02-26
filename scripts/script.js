98a30fd58a0ed7b89c688cc793a1f2a0745c6ea4
PauloHCruz <pauloh.g.j.j@gmail.com>
2025-02-26 17:08:47 -0300

    layout base

---
+++ scripts/script.js
@@ -0,0 +1,11 @@
+document.querySelector("#foco").addEventListener("click",function(){
+    document.body.style.backgroundColor = "#cc6666"
+});
+
+document.querySelector("#pausa-curta").addEventListener("click",function(){
+    document.body.style.backgroundColor = "#66cccc"
+});
+
+document.querySelector("#pausa-longa").addEventListener("click",function(){
+    document.body.style.backgroundColor = "#66cc99"
+});
